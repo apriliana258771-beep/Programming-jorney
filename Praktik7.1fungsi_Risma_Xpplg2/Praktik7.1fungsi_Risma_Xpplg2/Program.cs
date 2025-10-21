10efb530cabce449d099d4e2edc5d03d24bdd89d
apriliana258771-beep <apriliana258771@student.smkn1kandeman.sch.id>
2025-10-21 13:59:38 +0700

    Add files via upload

---
+++ Praktik7.1fungsi_Risma_Xpplg2/Praktik7.1fungsi_Risma_Xpplg2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Praktik7._1fungsi_Risma_Xpplg2
+{
+    internal class Program
+    {
+        static void Sapa()
+        {
+            Console.WriteLine("Halo, selamat datang di pembelajaran C#!");
+            Console.WriteLine("Mari belajar tentang fungsi!");
+        }
+        static void Main(string[] args)
+        {
+            Sapa(); // Memanggil funsi
+        }
+    }
+}
