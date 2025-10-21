abca24ea9983cf509617cbf46de940f8972c3022
apriliana258771-beep <apriliana258771@student.smkn1kandeman.sch.id>
2025-10-21 09:47:18 +0700

    Add files via upload

---
+++ Praktik6.5For risma xpplg2/Praktik6.5For risma xpplg2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Praktik6._5For_risma_xpplg2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // 1. Inisialisasi variabel penghitung (counter)
+            int angka = 1;
+
+            Console.WriteLine("Menampilkan angka 1 sampai 20:");
+
+            // 2. Blok 'do-while'
+            do
+            {
+                // Tampilkan nilai variabel 'angka' saat ini
+                Console.WriteLine(angka);
+
+                // Tambahkan nilai 'angka' sebanyak 1 (increment)
+                angka++;
+
+                // UJi kondisi: Loop berlanjut selama 'angka' kurang dari atau sama dengan
+            } while (angka <= 20);
+
+            Console.WriteLine("Selesai.");
+
+        }
+    }
+}
