e733fb56cb4b8ec386f733ec7f43352513269250
apriliana258771-beep <apriliana258771@student.smkn1kandeman.sch.id>
2025-10-21 09:48:42 +0700

    Add files via upload

---
+++ Praktik6.6For risma xpplg2/Praktik6.6For risma xpplg2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Praktik6._6For_risma_xpplg2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int angka;
+
+            // Blok 'do' akan dieksekusi minimal satu kali.
+            do
+            {
+                Console.Write("Masukkan angka genap: ");
+                // Mencoba membaca input dari pengguna
+                if (!int.TryParse(Console.ReadLine(), out angka))
+                {
+                    Console.WriteLine("Input tidak valid. Silakan masukkan angka.");
+                    // jika input tidak valid, set angka = 1 (ganjil) agar loop berlanjut
+                    angka = 1;
+                    continue; // lanjutkan ke iterasi berikutnya
+                }
+                // memeriksa apakah angka tersebut ganjil 
+                if (angka % 2 != 0)
+                {
+                    Console.WriteLine($"Angka {angka} adalah ganjil. Coba lagi. ");
+                }
+                // kondisi diperiksa di akhir. Loop berlanjut selama angka ganjil.
+            } while (angka % 2 != 0);
+            Console.WriteLine($"Selamat! Anda memasukkan angka genap: {angka}");
+
+        }
+        
+    }
+}
